c5429007020f702873b64135d0de4843756822c0
md-imrul <mmskw8@mail.umkc.edu>
2013-09-23 21:49:53 -0500

    lab 4 SOAP server and client added.

---
+++ lab 4/SOAPClient/SOAPClient/WebForm1.aspx
@@ -0,0 +1,27 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeBehind="WebForm1.aspx.cs" Inherits="SOAPClient.WebForm1" %>
+
+<!DOCTYPE html>
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+    <title></title>
+</head>
+<body>
+    <form id="form1" runat="server">
+    <div>
+    
+    </div>
+        Number 1:
+        <asp:TextBox ID="TBNum1" runat="server">0</asp:TextBox>
+        <br />
+        Number 2:
+        <asp:TextBox ID="TBNum2" runat="server">0</asp:TextBox>
+        <p>
+            <asp:Button ID="Button1" runat="server" Height="26px" OnClick="Button1_Click" Text="Calculate" />
+            <asp:Button ID="Button2" runat="server" OnClick="Button2_Click" style="margin-top: 0px" Text="Calculate(Remote)" />
+        </p>
+        Result:
+        <asp:TextBox ID="TBResult" runat="server"></asp:TextBox>
+    </form>
+</body>
+</html>
---
+++ lab 4/SOAPServer/App_Code/WebService.cs
@@ -0,0 +1,118 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Services;
+
+/// <summary>
+/// Summary description for WebService
+/// </summary>
+[WebService(Namespace = "http://tempuri.org/")]
+[WebServiceBinding(ConformsTo = WsiProfiles.BasicProfile1_1)]
+// To allow this Web Service to be called from script, using ASP.NET AJAX, uncomment the following line. 
+// [System.Web.Script.Services.ScriptService]
+public class WebService : System.Web.Services.WebService
+{
+
+    public WebService()
+    {
+
+        //Uncomment the following line if using designed components 
+        //InitializeComponent(); 
+    }
+
+    [WebMethod]
+    public string HelloWorld()
+    {
+        return "Hello World";
+    }
+
+    [WebMethod(Description = "Add two numbers.")]
+    public OpResponse Add(string x, string y)
+    {
+        int ix = (x != null) ? Int32.Parse(x) : 0;
+        int iy = (y != null) ? Int32.Parse(y) : 0;
+
+        return new OpResponse("Addition", ix, iy, ix + iy);
+    }
+
+    [WebMethod(Description = "Subtract y from x.")]
+    public OpResponse Subtract(string x, string y)
+    {
+        int ix = (x != null) ? Int32.Parse(x) : 0;
+        int iy = (y != null) ? Int32.Parse(y) : 0;
+
+        return new OpResponse("Subtract", ix, iy, ix - iy);
+    }
+
+    [WebMethod(Description = "Multiply two numbers.")]
+    public OpResponse Multiply(string x, string y)
+    {
+        int ix = (x != null) ? Int32.Parse(x) : 0;
+        int iy = (y != null) ? Int32.Parse(y) : 0;
+
+        return new OpResponse("Multiply", ix, iy, ix * iy);
+    }
+
+    [WebMethod(Description = "Divide x by y.")]
+    public OpResponse Divide(string x, string y)
+    {
+        int ix = (x != null) ? Int32.Parse(x) : 0;
+        int iy = (y != null) ? Int32.Parse(y) : 0;
+        int r = -1;
+        string error = null;
+        if (iy > 0) r = ix / iy;
+        else { error = "Divide by zero exception."; }
+        OpResponse response = new OpResponse("Divide", ix, iy, r);
+        if (error != null) response.Error = error;
+        return response;
+    }
+
+    public class OpResponse
+    {
+        int _result = 0;
+        int _x, _y;
+        string _opName;
+        string _error = null;
+
+        public OpResponse(string name, int x, int y, int r)
+        {
+            _opName = name;
+            _x = x;
+            _y = y;
+            _result = r;
+        }
+
+        public OpResponse() { _result = 0; }
+
+        public string OpName
+        {
+            get { return _opName; }
+            set { _opName = value; }
+        }
+        public int x
+        {
+            get { return _x; }
+            set { _x = value; }
+        }
+
+        public int y
+        {
+            get { return _y; }
+            set { _y = value; }
+        }
+
+        public int Result
+        {
+            get { return _result; }
+            set { _result = value; }
+        }
+
+        public string Error
+        {
+            get { return _error; }
+            set { _error = value; }
+        }
+    }
+}
+
