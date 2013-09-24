47d411db9ee68158af66fcf55f3b308550b08b44
md-imrul <mmskw8@mail.umkc.edu>
2013-09-24 00:02:14 -0500

    mobile client and report added.

---
+++ lab 4/WcfService3/WcfService3/IService1.cs
@@ -0,0 +1,105 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.ServiceModel;
+using System.ServiceModel.Web;
+using System.Text;
+
+namespace WcfService3
+{
+    // NOTE: You can use the "Rename" command on the "Refactor" menu to change the interface name "IService1" in both code and config file together.
+    [ServiceContract]
+    public interface IService1
+    {
+
+        [OperationContract]
+        OperationResponse Add(string v1, string v2);
+
+        [OperationContract]
+        OperationResponse Subtract(string v1, string v2);
+
+        [OperationContract]
+        OperationResponse Multiply(string v1, string v2);
+
+        [OperationContract]
+        OperationResponse Divide(string v1, string v2);
+
+        // TODO: Add your service operations here
+        //[OperationContract]
+        //AddResponse Addition(AddRequest request);
+    }
+
+
+    // Use a data contract as illustrated in the sample below to add composite types to service operations.
+    [DataContract]
+    public class CompositeType
+    {
+        bool boolValue = true;
+        string stringValue = "Hello ";
+
+        [DataMember]
+        public bool BoolValue
+        {
+            get { return boolValue; }
+            set { boolValue = value; }
+        }
+
+        [DataMember]
+        public string StringValue
+        {
+            get { return stringValue; }
+            set { stringValue = value; }
+        }
+    }
+
+
+    [DataContract(Name="AddResponse")]
+    public class OperationResponse {
+        int _result = 0;
+        int _x, _y;
+        string _opName = "";
+        string _errorMessage = "";
+
+        public OperationResponse(string opName, int x, int y, int r) {
+            _opName = opName;
+            _x = x; 
+            _y = y;
+            _result = r; 
+        }        
+
+        [DataMember]
+        public string OpName
+        {
+            get { return _opName; }
+            set { _opName = value; }
+        }
+
+        [DataMember]
+        public int x
+        {
+            get { return _x; }
+            set { _x = value; }
+        }
+
+        [DataMember]
+        public int y
+        {
+            get { return _y; }
+            set { _y = value; }
+        }
+
+        [DataMember]
+        public int result {
+            get { return _result; }
+            set { _result = value;}
+        }
+
+        [DataMember]
+        public string ErrorMessage
+        {
+            get { return _errorMessage; }
+            set { _errorMessage = value; }
+        }
+    }
+}
---
+++ lab 4/WcfService3/WcfService3/Service1.svc.cs
@@ -0,0 +1,76 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.ServiceModel;
+using System.ServiceModel.Web;
+using System.Text;
+
+namespace WcfService3
+{
+    // NOTE: You can use the "Rename" command on the "Refactor" menu to change the class name "Service1" in code, svc and config file together.
+    // NOTE: In order to launch WCF Test Client for testing this service, please select Service1.svc or Service1.svc.cs at the Solution Explorer and start debugging.
+    public class Service1 : IService1
+    {
+        
+        [WebInvoke (Method="GET", ResponseFormat=WebMessageFormat.Json, UriTemplate="add?x={v1}&y={v2}")]
+        public OperationResponse Add(string v1, string v2)
+        {
+            int x = ((v1 != null) ? Int32.Parse(v1) : 0);
+            int y = ((v2 != null) ? Int32.Parse(v2) : 0);
+            int result = x + y;
+            return new OperationResponse("addition", x, y, result);
+            
+        }
+
+        [WebInvoke(Method = "GET", ResponseFormat = WebMessageFormat.Json, UriTemplate = "subtract?x={v1}&y={v2}")]
+        public OperationResponse Subtract(string v1, string v2)
+        {
+            int x = ((v1 != null) ? Int32.Parse(v1) : 0);
+            int y = ((v2 != null) ? Int32.Parse(v2) : 0);
+            int result = x - y;
+            return new OperationResponse("subtract", x, y, result);
+
+        }
+
+        [WebInvoke(Method = "GET", ResponseFormat = WebMessageFormat.Json, UriTemplate = "multiply?x={v1}&y={v2}")]
+        public OperationResponse Multiply(string v1, string v2)
+        {
+            int x = ((v1 != null) ? Int32.Parse(v1) : 0);
+            int y = ((v2 != null) ? Int32.Parse(v2) : 0);
+            int result = x * y;
+            return new OperationResponse("multiply", x, y, result);
+
+        }
+
+        [WebInvoke(Method = "GET", ResponseFormat = WebMessageFormat.Json, UriTemplate = "divide?x={v1}&y={v2}")]
+        public OperationResponse Divide(string v1, string v2)
+        {
+            int x = ((v1 != null) ? Int32.Parse(v1) : 0);
+            int y = ((v2 != null) ? Int32.Parse(v2) : 0);
+            int result = -1; 
+            string error = null;
+            if(y > 0) result = x / y;
+            else {
+                error = "Divide by zero exception.";
+            }
+            OperationResponse r = new OperationResponse("divide", x, y, result);
+            if (error != null) r.ErrorMessage = error; 
+            return r;
+
+        }
+
+        public CompositeType GetDataUsingDataContract(CompositeType composite)
+        {
+            if (composite == null)
+            {
+                throw new ArgumentNullException("composite");
+            }
+            if (composite.BoolValue)
+            {
+                composite.StringValue += "Suffix";
+            }
+            return composite;
+        }
+    }
+}
