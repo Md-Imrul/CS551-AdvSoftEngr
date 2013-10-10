f8fbbbfbb2df86778004748ce1123865330ee941
md-imrul <mmskw8@mail.umkc.edu>
2013-10-09 23:27:20 -0500

    lab 3 documents revised.

---
+++ lab3/WcfService3/WcfService3/Service1.svc.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.ServiceModel;
+using System.ServiceModel.Web;
+using System.Text;
+
+namespace WcfService3
+{
+    // NOTE: You can use the "Rename" command on the "Refactor" menu to change the class name "Service1" in code, svc and config file together.
+    // NOTE: In order to launch WCF Test Client for testing this service, please select Service1.svc or Service1.svc.cs at the Solution Explorer and start debugging.
+    public class Service1 : IService1
+    {
+        public string GetData(int value)
+        {
+            return string.Format("You entered: {0}", value);
+        }
+
+        [WebInvoke (Method="GET", ResponseFormat=WebMessageFormat.Json, UriTemplate="add/{v1}/{v2}")]
+        public string Add(string v1, string v2)
+        {
+            int result = Int32.Parse(v1) + Int32.Parse(v2);
+            return "The summation is: " + (result);
+        }
+
+
+        public CompositeType GetDataUsingDataContract(CompositeType composite)
+        {
+            if (composite == null)
+            {
+                throw new ArgumentNullException("composite");
+            }
+            if (composite.BoolValue)
+            {
+                composite.StringValue += "Suffix";
+            }
+            return composite;
+        }
+    }
+}
